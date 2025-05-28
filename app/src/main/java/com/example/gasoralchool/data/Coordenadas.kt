66587673d7501b6c69c9aaa0ba8cc12d7b3d1103
Wesley Alves Rolim <walvesrolim@gmail.com>
2025-05-28 14:56:43 -0300

    ✨ save switch state

--- app/src/main/java/com/example/gasoralchool/data/Coordenadas.kt
+++
@@ -1,3 +0,0 @@
-package com.example.gasoralchool.data
-
-data class Coordenadas(val latitude: Double, val longitude: Double)
--- app/src/main/java/com/example/gasoralchool/view/EthanolOrGas.kt
+++ app/src/main/java/com/example/gasoralchool/view/EthanolOrGas.kt
@@ -10,10 +10,8 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.Button
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -28,18 +26,25 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.example.gasoralchool.R
+import com.example.gasoralchool.models.userPreferences.UserPreferences
+import com.example.gasoralchool.models.userPreferences.UserPreferencesRepository
 
 @Composable
-fun AlcoolGasolinaPreco(navController: NavHostController) {
-  var alcool by remember { mutableStateOf("") }
+fun EthanolOrGas(navController: NavHostController) {
+  val context = LocalContext.current
+  val userPreferences = UserPreferencesRepository(context)
+
+  var ethanol by remember { mutableStateOf("") }
   var gasolina by remember { mutableStateOf("") }
   var nomeDoPosto by remember { mutableStateOf("") }
-  var checkedState by remember { mutableStateOf(true) }
+  var checkedState by remember { mutableStateOf(userPreferences.read().carEfficiencyIs75) }
 
   // A surface container using the 'background' color from the theme
   Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
@@ -50,8 +55,8 @@ fun AlcoolGasolinaPreco(navController: NavHostController) {
     ) {
       // Campo de texto para entrada do preço
       OutlinedTextField(
-        value = alcool,
-        onValueChange = { alcool = it }, // Atualiza o estado
+        value = ethanol,
+        onValueChange = { ethanol = it }, // Atualiza o estado
         label = { Text("Preço do Álcool (R$)") },
         modifier = Modifier.fillMaxWidth(), // Preenche a largura disponível
         keyboardOptions =
@@ -86,10 +91,12 @@ fun AlcoolGasolinaPreco(navController: NavHostController) {
         Switch(
           modifier = Modifier.semantics { contentDescription = "Demo with icon" },
           checked = checkedState,
-          onCheckedChange = { checkedState = it },
+          onCheckedChange = {
+            checkedState = it
+            userPreferences.save(UserPreferences(checkedState))
+          },
           thumbContent = {
             if (checkedState) {
-              // Icon isn't focusable, no need for content description
               Icon(
                 imageVector = Icons.Filled.Check,
                 contentDescription = null,
@@ -108,14 +115,6 @@ fun AlcoolGasolinaPreco(navController: NavHostController) {
         style = MaterialTheme.typography.bodyMedium,
         modifier = Modifier.padding(top = 16.dp),
       )
-      Row(
-        modifier = Modifier.fillMaxWidth().padding(16.dp),
-        horizontalArrangement = Arrangement.End,
-      ) {
-        FloatingActionButton(onClick = { navController.navigate("ListaDePostos/$nomeDoPosto") }) {
-          Icon(Icons.Filled.Add, "Inserir Posto")
-        }
-      }
     }
   }
 }
