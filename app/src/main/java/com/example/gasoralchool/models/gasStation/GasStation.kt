e91ee51fbf4d568823387f08d8a24471d0bd1e3c
Wesley Alves Rolim <walvesrolim@gmail.com>
2025-06-02 15:20:27 -0300

    ♻️ add `createdAt` field

--- app/src/main/java/com/example/gasoralchool/models/gasStation/GasStation.kt
+++ app/src/main/java/com/example/gasoralchool/models/gasStation/GasStation.kt
@@ -1,13 +1,18 @@
 package com.example.gasoralchool.models.gasStation
 
 import java.io.Serializable
+import java.time.OffsetDateTime
+import java.time.ZoneOffset
+import java.time.temporal.ChronoUnit
 import java.util.UUID
+import java.util.concurrent.ThreadLocalRandom
 
 data class GasStation(
   val id: String = UUID.randomUUID().toString(),
   val name: String?,
   val fuels: List<Fuel>,
   val coordinates: Coordinates = Coordinates(41.40338, 2.17403),
+  val createdAt: String = getCurrentTime(),
 ) : Serializable {
   init {
     if (fuels.size < 2) throw Exception("Both gas and ethanol fuels are required")
@@ -20,4 +25,13 @@ data class GasStation(
     val priceProportionBetweenFuels = ethanol.price / gas.price
     return if (priceProportionBetweenFuels > proportionToCompare) gas else ethanol
   }
+}
+
+fun getCurrentTime(): String {
+  val utc = ZoneOffset.UTC
+  val today = OffsetDateTime.now(utc).truncatedTo(ChronoUnit.SECONDS)
+  val seventyYearsAgo = today.minusYears(70)
+  val totalSeconds = ChronoUnit.SECONDS.between(seventyYearsAgo, today)
+  val date = today.minusSeconds(ThreadLocalRandom.current().nextLong(0, totalSeconds))
+  return date.toString()
 }
